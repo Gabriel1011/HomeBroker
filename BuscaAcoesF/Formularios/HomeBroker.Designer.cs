7ddbc1da278fb4f9248774cfca3c89f88664ba65
Gabriel Rodrigues de Almeida <G.ALMEIDA@btp.com.br>
2020-04-18 21:07:29 -0300

    - ajuste de grid

--- BuscaAcoesF/Formularios/HomeBroker.Designer.cs
+++ BuscaAcoesF/Formularios/HomeBroker.Designer.cs
@@ -223,9 +223,6 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.Black;
